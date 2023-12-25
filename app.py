021477128cb364a026a71568425d63a5cd38e659
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2023-12-25 22:25:21 +0100

    Add parameterized endpoint feature

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from markupsafe import escape
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
